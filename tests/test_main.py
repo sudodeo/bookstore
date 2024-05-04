d54d1ff483e2b033f0a9754043782b62d563f9dd
sudodeo <adesolaadeoluwa@gmail.com>
2024-05-04 12:30:28 +0100

    remove comments and unused imports

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,29 +5,11 @@ import pytest
 
 root_path = Path(__file__).resolve().parent.parent
 sys.path.append(str(root_path))
-# from fastapi.testclient import TestClient
-# from src.main import app
-
-# client = TestClient(app)
-
-
-# @pytest.fixture(scope="module")
-# def test_app():
-#     client = TestClient(app)
-#     yield client
-
-
-# def test_read_main():
-#     response = client.get("/")
-#     assert response.status_code == 200
-#     assert response.json() == {"Documentation endpoint": "/docs"}
 
 import pytest
 from fastapi.testclient import TestClient
 from src.main import app
-from src.database.db import get_test_session, create_test_database, drop_test_database
-
-# client = TestClient(app)
+from src.database.db import drop_test_database
 
 
 @pytest.fixture(scope="module")
