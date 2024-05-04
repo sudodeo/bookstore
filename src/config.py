109daac8aa9a505e903af4dd0c670258602ffed2
sudodeo <adesolaadeoluwa@gmail.com>
2024-05-04 12:32:38 +0100

    refactor to accommodate testing

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict, BaseSettings
 
 
 class Settings(BaseSettings):
     POSTGRESQL_CONN_STRING: str
-
-    class Config:
-        env_file = ".env"
+    POSTGRESQL_TEST_CONN_STRING: str
+    APP_ENV: str = "dev"
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
--- src/database/db.py
+++ src/database/db.py
@@ -1,18 +1,33 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from ..config import settings
 
 engine = create_engine(settings.POSTGRESQL_CONN_STRING)
+test_engine = create_engine(settings.POSTGRESQL_TEST_CONN_STRING)
+if settings.APP_ENV == "test":
+    engine = test_engine
+TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 
 def get_db():
-    db = SessionLocal()
+    if settings.APP_ENV == "test":
+        db = TestSessionLocal()
+    else:
+        db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+def get_test_session():
+    return TestSessionLocal()
+
+
+def drop_test_database():
+    Base.metadata.drop_all(bind=test_engine)
